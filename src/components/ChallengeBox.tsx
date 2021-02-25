90eea045b2561523dcc3fc632355f75e0de8f3f2
Lucas Barroso <lucasbarroso23@gmail.com>
2021-02-25 10:50:39 -0300

    challengeBox component

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,40 @@
+import styles from "../styles/components/ChallengeBox.module.css";
+
+export function ChallengeBox() {
+  const hasActiveChallenge = true;
+
+  return (
+    <div className={styles.challengeBoxContainer}>
+      {hasActiveChallenge ? (
+        <div className={styles.challengeActive}>
+          <header>Ganhe 400 XP</header>
+
+          <main>
+            <img src="icons/body.svg" alt="Body" />
+            <strong>Novo desafio</strong>
+            <p>Levante e faça 10 polichinelos</p>
+          </main>
+
+          <footer>
+            <button type="button" className={styles.challengeFailedButton}>
+              Falhei
+            </button>
+            <button type="button" className={styles.challengeSucceededButton}>
+              Completei
+            </button>
+          </footer>
+        </div>
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <strong>
+            Inicie um ciclo para receber desafios a serem completados
+          </strong>
+          <p>
+            <img src="icons/level-up.svg" alt="Level Up" />
+            Avance de level completando desafios.
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
+import Head from "next/head";
 import { CompletedChallanges } from "../components/CompletedChallanges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
 
 import styles from "../styles/pages/Home.module.css";
-
-import Head from "next/head";
 
 export default function Home() {
   return (
@@ -22,7 +22,9 @@ export default function Home() {
           <Countdown />
         </div>
 
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
